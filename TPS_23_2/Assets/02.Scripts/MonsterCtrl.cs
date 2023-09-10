5084029d5c258534bb9393cb6d89914fb86be7ef
mmeo009 <shh020615@gmail.com>
2023-09-11 00:27:22 +0900

    2주차

--- TPS_23_2/Assets/02.Scripts/MonsterCtrl.cs
+++ TPS_23_2/Assets/02.Scripts/MonsterCtrl.cs
@@ -46,12 +46,15 @@ public class MonsterCtrl : MonoBehaviour
     private int hp = 100;
 
 
-
     // 스크립트가 활성화될 때마다 호출되는 함수
     void OnEnable()
     {
         // 이벤트 발생 시 수행할 함수 연결
         PlayerCtrl.OnPlayerDie += this.OnPlayerDie;
+        // 몬스터의 상태를 체크하는 코루틴 함수 호출
+        StartCoroutine(CheckMonsterState());
+        // 상태에 따라 몬스터의 행동을 수행하는 코루틴 함수 호출
+        StartCoroutine(MonsterAction());
     }
 
 
@@ -66,30 +69,19 @@ public class MonsterCtrl : MonoBehaviour
 
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         // 몬스터의 Transform 할당
         monsterTr = GetComponent<Transform>();
-
         // 추적 대상인 Player의 Transform 할당
         playerTr = GameObject.FindWithTag("PLAYER").GetComponent<Transform>();
-
         // NavMeshAgent 컴포넌트 할당
         agent = GetComponent<NavMeshAgent>();
-
         // Animator 컴포넌트 할당
         anim = GetComponent<Animator>();
-
         // BloodSprayEffect 프리팹 로드
         bloodEffect = Resources.Load<GameObject>("BloodSprayEffect");
-
-        // 몬스터의 상태를 체크하는 코루틴 함수 호출
-        StartCoroutine(CheckMonsterState());
-
-        // 상태에 따라 몬스터의 행동을 수행하는 코루틴 함수 호출
-        StartCoroutine(MonsterAction());
     }
-
 
 
     // 일정한 간격으로 몬스터의 행동 상태를 체크
@@ -166,21 +158,27 @@ public class MonsterCtrl : MonoBehaviour
                 // 사망
                 case State.DIE:
                     isDie = true;
-
                     // 추적 정지
                     agent.isStopped = true;
-
                     // 사망 애니메이션 실행
                     anim.SetTrigger(hashDie);
-
                     // 몬스터의 Collider 컴포넌트 비활성화
                     GetComponent<CapsuleCollider>().enabled = false;
+                    // 일정 시간 대기 후 오브젝트 풀링으로 환원
+                    yield return new WaitForSeconds(3.0f);
+                    // 사망 후 다시 사용할 때를 위해 hp 값 초기화
+                    hp = 100;
+                    isDie = false;
+                    state = State.IDLE;
+                    // 몬스터의 Collider 컴포넌트 활성화
+                    GetComponent<CapsuleCollider>().enabled = true;
+                    // 몬스터를 비활성화
+                    this.gameObject.SetActive(false);
                     break;
             }
             yield return new WaitForSeconds(0.3f);
         }
     }
-
 
 
     void OnDrawGizmos()
@@ -207,30 +205,29 @@ public class MonsterCtrl : MonoBehaviour
         {
             // 충돌한 총알을 삭제
             Destroy(coll.gameObject);
-            // 피격 리액션 애니메이션 실행
-            anim.SetTrigger(hashHit);
+        }
+    }
 
-
-            // 총알의 충돌 지점
-            Vector3 pos = coll.GetContact(0).point;
-            // 총알의 충돌 지점의 법선 벡터
-            Quaternion rot = Quaternion.LookRotation(-coll.GetContact(0).normal);
-            // 혈흔 효과를 생성하는 함수 호출
-            ShowBloodEffect(pos, rot);
-
-
-            // 몬스터의 hp 차감
-            hp -= 10;
-
-            if (hp <= 0)
-            {
-                state = State.DIE;
-            }
+    // 레이캐스트를 사용해 데미지를 입히는 로직
+    public void OnDamage(Vector3 pos, Vector3 normal)
+    {
+        // 피격 리액션 애니메이션 실행
+        anim.SetTrigger(hashHit);
+        Quaternion rot = Quaternion.LookRotation(normal);
+        // 혈흔 효과를 생성하는 함수 호출
+        ShowBloodEffect(pos, rot);
+        // 몬스터의 hp 차감
+        hp -= 30;
+        if (hp <= 0)
+        {
+            state = State.DIE;
+            // 몬스터가 사망했을 때 50점을 추가
+            GameManager.instance.DisplayScore(50);
         }
     }
 
 
-    
+
     // 자기 자신 충돌 감지를 위해서 추가
     void OnTriggerEnter(Collider coll)
     {
